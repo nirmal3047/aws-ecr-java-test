1d30b6a90c02c93d0de6a27d93cbded88d57ab09
Nirmal Kumar <nirmal3047@gmail.com>
2021-03-11 10:04:48 +0530

    add test

---
+++ service/src/test/java/com/ezyreach/bootstrap/BootstrapAppTest.java
@@ -0,0 +1,16 @@
+package com.ezyreach.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BootstrapAppTest {
+
+    @Test
+    public void contextLoad() {
+
+    }
+
+}
